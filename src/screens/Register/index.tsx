e4aa26d5faf3979cf7a4465315e93e95bb7e905b
Samuel Alves <samuellmsl05@gmail.com>
2021-09-30 16:38:39 -0300

    :construction: Botões na tela de login

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Input } from '../../components/Form/Input';
 import { Button } from '../../components/Form/Button';
+import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
 
 import {
     Container,
     Header,
     Title,
     Form,
-    Fields
+    Fields,
+    TransactionsTypes
 } from './styles';
 
 export function Register() {
+    const [transactionType, setTransactionType] = useState('');
+
+    function handleTransactionTypeSelect(type: 'up' | 'down') {
+        setTransactionType(type);
+    }
+
     return (
         <Container>
             <Header>
@@ -23,6 +31,20 @@ export function Register() {
                     <Input placeholder="Nome" />
                     <Input placeholder="Preço" />
 
+                    <TransactionsTypes>
+                        <TransactionTypeButton
+                            type="up"
+                            title="income"
+                            onPress={() => handleTransactionTypeSelect('up')}
+                            isActive={transactionType === 'up'}
+                        />
+                        <TransactionTypeButton
+                            type="down"
+                            title="outcome"
+                            onPress={() => handleTransactionTypeSelect('down')}
+                            isActive={transactionType === 'down'}
+                        />
+                    </TransactionsTypes>
                 </Fields>
                 <Button title="Enviar" />
             </Form>
